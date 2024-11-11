22e3b1077eeaf60f9b77c21dc54f22d103a667aa
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2024-11-11 18:15:41 +0530

    Implement counter 2 problem

---
+++ counter2.js
@@ -0,0 +1,25 @@
+/*
+  Problem - Write a function createCounter.
+  It should accept an initial integer init.
+  It should return an object with three functions.
+
+  The three functions are:
+
+  increment() increases the current value by 1 and then returns it.
+  
+  decrement() reduces the current value by 1 and then returns it.
+  
+  reset() sets the current value to init and then returns it.
+*/
+
+const createCounter = (val) => {
+  let counter = val;
+  return {
+    increment: () => ++counter,
+    decrement: () => --counter,
+    reset: () => {
+      counter = val;
+      return counter;
+    },
+  };
+};
