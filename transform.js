6a0c8b51ef6a0de60a23ae3afcc0ea0594ee51c1
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2024-11-11 18:42:54 +0530

    Implement transform problem

---
+++ transform.js
@@ -0,0 +1,18 @@
+/*
+  Problem - Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+  The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+  Please solve it without the built-in Array.map method.
+*/
+
+const args = require('./inputs');
+const [array, func] = args;
+
+const transform = (arr, fn) => {
+  const transformedArray = [];
+  for (let val of arr) {
+    transformedArray.push(fn(val));
+  }
+  return transformedArray;
+};
+
+// console.log(transform(JSON.parse(array), eval(func)));
