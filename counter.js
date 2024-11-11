46787aca22dba1add9ff54ab2fac1c9a9cc12a45
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2024-11-11 17:58:21 +0530

    Implement counter problem

---
+++ counter.js
@@ -0,0 +1,14 @@
+/*
+  Problem - Given an integer n, return a counter function.
+  This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+*/
+
+const args = require('./inputs');
+
+const counter = () => {
+  let count = args[0];
+  return () => {
+    return count++;
+  };
+};
+
