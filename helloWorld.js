28877700f72fa84d201cdbdaa1c2199372563d08
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2024-11-11 17:50:23 +0530

    Implement hello world problem

---
+++ helloWorld.js
@@ -0,0 +1,7 @@
+/*
+  Problem - Write a function createHelloWorld.
+  It should return a new function that always returns "Hello World".
+*/
+
+const helloWorld = () => (...args) => 'Hello World';
+
