35ae6d43e5d9d002c9d64a5d739e36b2d1ef0ba1
Rohan Gupta <rohan.gupta@gameskraft.com>
2024-11-12 10:46:55 +0530

    Implement arguments length problem

---
+++ argumentsLength.js
@@ -0,0 +1,9 @@
+/*
+  Problem - Write a function argumentsLength that returns the count of arguments passed to it.
+*/
+
+const arguments = require('./inputs');
+
+const argumentsLength = (...args) => args?.length || 0;
+
+console.log(argumentsLength(...arguments));
