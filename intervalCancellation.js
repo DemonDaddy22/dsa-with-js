4fa792379cd905ae4063c4f405cf7570ef3c71c7
Rohan Gupta <rohan.gupta@gameskraft.com>
2024-11-12 12:35:04 +0530

    Implement interval cancellation problem

---
+++ intervalCancellation.js
@@ -0,0 +1,16 @@
+/*
+  Problem - Given a function fn, an array of arguments args, and an interval time t, return a cancel function cancelFn.
+
+  After a delay of cancelTimeMs, the returned cancel function cancelFn will be invoked.
+
+  setTimeout(cancelFn, cancelTimeMs)
+  The function fn should be called with args immediately and then called again every t milliseconds until cancelFn is called at cancelTimeMs ms.
+*/
+
+const cancellable = (fn, args, t) => {
+  fn(...args);
+  const id = setInterval(() => fn(...args), t);
+  return () => {
+    clearTimeout(id);
+  };
+};
