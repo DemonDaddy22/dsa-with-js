ae7b6db9c244a8f37e5654c16fb85d623a0ba1d7
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2024-11-17 17:37:43 +0530

    Implement arithmetic progression problem

---
+++ arithmeticProgression.js
@@ -0,0 +1,17 @@
+/*
+  Problem - A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
+
+  Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression.
+  Otherwise, return false.
+*/
+
+const canMakeArithmeticProgression = (arr) => {
+  const sortedArray = arr.sort();
+  let difference = Math.abs(sortedArray[1] - sortedArray[0]);
+  for (let i = 2; i < sortedArray.length; i++) {
+    if (Math.abs(sortedArray[i] - sortedArray[i - 1]) !== difference) {
+      return false;
+    }
+  }
+  return true;
+};
